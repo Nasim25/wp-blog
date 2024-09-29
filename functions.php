7556ddeb8d377202c976a983545daf1fd5fba491
nasim114 <nasimuddin1140@gmail.com>
2024-09-30 00:42:48 +0600

    css and js added

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+function nasim_blog_script_css()
+{
+    wp_enqueue_style('google-font', 'https://fonts.googleapis.com/css?family=Droid+Sans:400,700');
+    wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.css', array(), 'all');
+    wp_enqueue_style('font-awesome', get_template_directory_uri() . '/css/font-awesome.min.css', array(), 'all');
+    wp_enqueue_style('main-css', get_template_directory_uri() . '/css/style.css', array(), 'all');
+    wp_enqueue_style('responsive-css', get_template_directory_uri() . '/css/responsive.css', array(), 'all');
+    wp_enqueue_style('colors-css', get_template_directory_uri() . '/css/colors.css', array(), 'all');
+    wp_enqueue_style('garden-css', get_template_directory_uri() . '/css/version/garden.css', array(), 'all');
+
+    wp_enqueue_script('tether-js', get_template_directory_uri() . '/js/tether.min.js', array(), 'all');
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), 'all');
+    wp_enqueue_script('custom-js', get_template_directory_uri() . '/js/custom.js', array(), 'all');
+    // <script src="js/jquery.min.js"></script>
+}
+
+add_action('wp_enqueue_scripts', 'nasim_blog_script_css');
--- index.php
+++ index.php
@@ -17,35 +17,10 @@
     <!-- Site Icons -->
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
     <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
-    
-    <!-- Design fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet"> 
 
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-
-    <!-- FontAwesome Icons core CSS -->
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="style.css" rel="stylesheet">
-
-    <!-- Responsive styles for this template -->
-    <link href="css/responsive.css" rel="stylesheet">
-
-    <!-- Colors for this template -->
-    <link href="css/colors.css" rel="stylesheet">
-
-    <!-- Version Garden CSS for this template -->
-    <link href="css/version/garden.css" rel="stylesheet">
-
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+    <?php wp_head() ?>
 </head>
-<body>
+<body <?php body_class() ?>>
 
     <div id="wrapper">
         <div class="collapse top-search" id="collapseExample">
@@ -524,12 +499,6 @@
         
     </div><!-- end wrapper -->
 
-    <!-- Core JavaScript
-    ================================================== -->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/tether.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/custom.js"></script>
-
+    <?php wp_footer() ?>
 </body>
 </html>
